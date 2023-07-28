d214ab488077fdca609caa84c30b94081ee0ca26
qwsderf23 <128019754+qwsderf23@users.noreply.github.com>
2023-07-28 16:04:20 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,15 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCtYrWXmivup2A2FWnjt7719u0asW5i1nw",
+  authDomain: "sty-yty.firebaseapp.com",
+  databaseURL: "https://sty-yty-default-rtdb.firebaseio.com",
+  projectId: "sty-yty",
+  storageBucket: "sty-yty.appspot.com",
+  messagingSenderId: "965934940248",
+  appId: "1:965934940248:web:d8b1b545a9b90302a14a2d",
+  measurementId: "G-R3YV357YCT"
+};
+
+
+// initialize firebase
+firebase.initializeApp(firebaseConfig);
